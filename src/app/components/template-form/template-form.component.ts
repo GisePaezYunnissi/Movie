127bc955577ac1df394d52c9bbab8adee77230ab
gisellapaezyunnissi <gisesabri@hotmail.com>
2021-12-04 00:17:17 -0300

    desafioFormulario

---
+++ src/app/components/template-form/template-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/models/persona.model';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.scss']
+})
+export class TemplateFormComponent implements OnInit {
+
+  persona: Persona = {
+    nombre: 'Juan',
+    apellido:'Perez',
+    edad: 26,
+    email:'',
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  agregarPersona(datos: any){
+    console.log(datos);
+  }
+
+}
